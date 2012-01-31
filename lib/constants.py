43be913b42eeea343245b0f81be5aa80dc4532cc
yendo0206 <yendo0206@localhost>
2012-01-31 21:53:48 +0900

    Added Japanese translation.

--- lib/constants.py
+++ lib/constants.py
@@ -5,7 +5,7 @@ from stat import S_IMODE
 
 from xdg.BaseDirectory import *
 
-VERSION = '0.1'
+VERSION = '0.2-a1'
 APP_NAME = 'gfeedline'
 
 SHARED_DATA_DIR = abspath(os.path.join(dirname(__file__), '../share'))
