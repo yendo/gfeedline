1fddf842a0428bface73a6d96045b16f0749cda7
yendo0206 <yendo0206@localhost>
2012-02-06 15:54:00 +0900

    Added support for actions on notification.

--- lib/utils/notification.py
+++ lib/utils/notification.py
@@ -17,23 +17,32 @@ class Notification(object):
                 'org.freedesktop.Notifications',
                 '/org/freedesktop/Notifications', 
                 'org.freedesktop.Notifications', None)
+
+            self.proxy.connect('g-signal', self.on_dbus_signal)
         except:
             print "Exception: %s" % sys.exc_info()[1]
 
-    def notify(self, icon, summary, body):
-        # print type(body)
-
-        #hints = {'urgency':  GLib.Variant.new_int16(1),
-        #         'category': GLib.Variant.new_string('im.received')}
-        hints = {}
+    def notify(self, icon, summary, body, entry=None):
+        actions = self._get_actions(entry)
+        hints = self._get_hints(entry)
 
         self.proxy.Notify(
             '(susssasa{sv}i)', 
-            self.app_name, 0, icon, summary, body, [], hints, -1)
+            self.app_name, 0, icon, summary, body, actions, hints, -1)
 
     def get_capabilities(self):
         return self.proxy.GetCapabilities()
 
+    def on_dbus_signal(self, proxy, sender_name, signal_name, params):
+        pass
+
+    def _get_actions(self, entry):
+        return []
+
+    def _get_hints(self, entry):
+        #hints = {'urgency':  GLib.Variant.new_int16(1),
+        #         'category': GLib.Variant.new_string('im.received')}
+        return {}
 
 if __name__ == '__main__':
     n = Notification('app')
