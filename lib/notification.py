a19e89d56be76df6c1e03872efa7a6b83e001a42
yendo0206 <yendo0206@localhost>
2012-02-14 09:31:08 +0900

    Code refactoring.

--- lib/notification.py
+++ lib/notification.py
@@ -18,41 +18,40 @@ class StatusNotification(Notification):
     def __init__(self, notification):
         super(StatusNotification, self).__init__('Gnome Feed Line')
         self.has_actions = 'actions' in self.get_capabilities()
+        self.icon_file = os.path.join(TMP_DIR, 'notification_icon.jpg')
 
     def notify(self, entry):
         icon_uri = str(entry['image_uri'])
-        entry['icon_path'] = os.path.join(TMP_DIR, 'notification_icon.jpg')
- 
+
         urlget = UrlGetWithAutoProxy(icon_uri)
-        d = urlget.downloadPage(icon_uri, entry['icon_path']).\
+        d = urlget.downloadPage(icon_uri, self.icon_file).\
             addCallback(self._notify, entry).addErrback(self._error)
  
     def _notify(self, unknown, entry):
         super(StatusNotification, self).notify(
-            entry['icon_path'], entry['user_name'], entry['popup_body'], entry)
+            self.icon_file, entry['user_name'], entry['popup_body'], entry)
 
     def on_dbus_signal(self, proxy, sender_name, signal_name, params):
         if signal_name == "ActionInvoked":
-            id, action_string = params.unpack()
-            action, user, id = action_string.split(' ')
+            notify_id, action_string = params.unpack()
+            action, user, entry_id = action_string.split(' ')
             
             if action == 'reply':
-                print "replay"
-                update_window = UpdateWindow(None, user, id)
-        
+                pass
+#                entry_dict = self.all_entries[ int(entry_id)]
+#                update_window = UpdateWindow(None, entry_dict)
             elif action == 'open':
-                uri = 'https://twitter.com/%s/status/%s' % (user, id)
-                print "open"
+                uri = 'https://twitter.com/%s/status/%s' % (user, entry_id)
                 webbrowser.open(uri)
 
     def _get_actions(self, entry):
         #print entry
         if self.has_actions:
             user = entry['user_name']
-            id = entry['id']
+            entry_id = entry['id']
 
-            actions = ['reply %s %s' % (user, id), _('Reply'),
-                       'open %s %s'  % (user, id), _('Open')]
+            actions = ['reply %s %s' % (user, entry_id), _('Reply'),
+                       'open %s %s'  % (user, entry_id), _('Open')]
         else:
             actions = []
 
