03e7913bba96920409ed50dff30d332efc3e0a17
yendo0206 <yendo0206@localhost>
2012-02-28 12:20:20 +0900

    Code refactoring.

--- lib/preferences/filters.py
+++ lib/preferences/filters.py
@@ -17,51 +17,35 @@ class FilterDialog(object):
         self.parent = parent
         self.liststore_row = liststore_row
 
-        self.combobox_target = self.gui.get_object('comboboxtext_target')
-        self.combobox_target.set_active(0)
-        self.combobox_model = self.combobox_target.get_model()
+        self.combobox_target = ComboboxTarget(self.gui)
         self.entry_word = self.gui.get_object('entry_word')
         self.spinbutton_expiry = self.gui.get_object('spinbutton_expiry')
-        self.combobox_expire_unit = self.gui.get_object('comboboxtext_expire_unit')
+        self.combobox_expire_unit = ComboboxExpireUnit(self.gui)
 
-
-#        self.on_comboboxtext_target_changed()
         self.gui.connect_signals(self)
 
     def run(self):
         dialog = self.gui.get_object('filter_dialog')
         dialog.set_transient_for(self.parent)
-        self.combobox_expire_unit.set_active(0) # glade bug
-
-        #source_widget = SourceComboBox(self.gui, source_list, self.data)
 
         if self.liststore_row:
-
-            labels = [i[0] for i in self.combobox_model]
-            target = labels.index(self.liststore_row[0])
-            self.combobox_target.set_active(target)
+            self.combobox_target.set_active_text(self.liststore_row[0])
             self.entry_word.set_text(self.liststore_row[1])
-
-            active = self.liststore_row[3] == "hours"
-            self.combobox_expire_unit.set_active(active)
             self.spinbutton_expiry.set_value(int(self.liststore_row[2]))
+            self.combobox_expire_unit.set_active_text(self.liststore_row[3])
 
         # run
         response_id = dialog.run()
 
-        expire_time_value = self.spinbutton_expiry.get_value_as_int()
-        now = datetime.now()
-        expire_days = timedelta(days=expire_time_value)
-        future = now + expire_days
-        expire_epoch = int(time.mktime(future.timetuple()))
-
-        expiration_unit = 'hours' \
-            if self.combobox_expire_unit.get_active() else 'days'
+        expire_time = ExpireTime(self.spinbutton_expiry.get_value_as_int(),
+                                 self.combobox_expire_unit.get_active())
 
         v = [
-            self.combobox_model[self.combobox_target.get_active()][0],
+            self.combobox_target.get_active_text(),
             self.entry_word.get_text().decode('utf-8'),
-            str(expire_time_value), expiration_unit, expire_epoch,
+            str(self.spinbutton_expiry.get_value_as_int()), 
+            self.combobox_expire_unit.get_active_text(), 
+            expire_time.get_epoch(),
         ]
 
 #        print v
@@ -69,6 +53,52 @@ class FilterDialog(object):
 #        if response_id == Gtk.ResponseType.OK:
 #            SETTINGS_RECENTS.set_string('source', v['source'])
         return response_id , v
+
+class ExpireTime(object):
+
+    def __init__(self, expire_timedelta, is_hours):
+        self.expire_timedelta = expire_timedelta
+        if not is_hours:
+            self.expire_timedelta *= 24
+
+    def get_epoch(self):
+        now = datetime.now()
+        future = now + self._get_timedelta()
+        expire_epoch = int(time.mktime(future.timetuple()))
+
+    def _get_timedelta(self):
+        return timedelta(hours=self.expire_timedelta)
+
+class ComboboxTarget(object):
+
+    WIDGET = 'comboboxtext_target'
+
+    def __init__(self, gui):
+        self.widget = gui.get_object(self.WIDGET)
+        self.widget.set_active(0) # glade bug
+        self.model = self.widget.get_model()
+
+    def get_active(self):
+        return self.widget.get_active()
+
+    def get_active_text(self):
+        return self.model[self.widget.get_active()][0]
+
+    def set_active_text(self, text):
+        labels = [i[0] for i in self.model]
+        target = labels.index(text)
+        self.widget.set_active(target)
+
+class ComboboxExpireUnit(ComboboxTarget):
+
+    WIDGET = 'comboboxtext_expire_unit'
+
+    def get_active_text(self):
+        return 'hours' if self.widget.get_active() else 'days'
+
+    def set_active_text(self, text):
+        active = text == "hours"
+        self.widget.set_active(active)
 
 class FilterListStore(Gtk.ListStore):
 
