dc9a762f1e7663e9c544f54287b23fdb41764b13
yendo0206 <yendo0206@localhost>
2012-11-06 18:21:19 +0900

    Removed a debug message.

--- lib/utils/urlgetautoproxy.py
+++ lib/utils/urlgetautoproxy.py
@@ -43,7 +43,6 @@ def urlget_with_autoproxy(url, arg=None, cb=None, **kargs):
     return d
 
 def urlpost_with_autoproxy(url, arg, cb=None, method='POST'):
-    print method, type(method)
     client = UrlGetWithAutoProxy(url)
     content_type = {'Content-Type' : 'application/x-www-form-urlencoded'}
 
