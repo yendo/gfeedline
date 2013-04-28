2c4165e2711161917dfe78e54158ff73b3d32c98
yendo0206 <yendo0206@localhost>
2013-04-28 23:41:15 +0900

    Fixed a URL for POST statuses/update_with_media.

--- lib/plugins/twitter/account.py
+++ lib/plugins/twitter/account.py
@@ -105,7 +105,7 @@ class Twitter(twitter.Twitter):
             fields += params.items()
 
         return self.__postMultipart(
-            'https://upload.twitter.com/1/statuses/update_with_media.json',
+            'https://api.twitter.com/1.1/statuses/update_with_media.json',
             fields=tuple(fields))
 
     def update_token(self, token):
