878d997833b30a20e681233acba7052606cd45dd
yendo0206 <yendo0206@localhost>
2013-05-24 22:20:18 +0900

    2.2 b1 release.

--- lib/constants.py
+++ lib/constants.py
@@ -4,7 +4,7 @@ from stat import S_IMODE
 
 from xdg.BaseDirectory import *
 
-VERSION = '2.2-a2'
+VERSION = '2.2-b1'
 APP_NAME = 'gfeedline'
 
 SHARED_DATA_DIR = abspath(os.path.join(dirname(__file__), '../share'))
